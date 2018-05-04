e74e411db1fea8364b8f69cf8a97b50637e4f77b
Andrea Giulianini <andreagiulianini97@gmail.com>
2018-05-04 20:36:20 +0200

    Add files via upload

--- UDPclient.c
+++ UDPclient.c
@@ -6,102 +6,130 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <pthread.h>
 
 #include "myfunction.h"
 
 #define MAX_BUF_SIZE 1024 // Maximum size of UDP messages
 
+//Variable for connection to server, ip and port
+int SERVER_PORT;
+char *SERVER_IP;
+//Now start configuration parameters for client
+struct sockaddr_in
+        server_addr; // Struct containing server address information
+
+struct sockaddr_in
+        client_addr; // Struct containing client address information
+
+int sfd; // Server socket filed descriptor
+
+int br; // Bind result
+
+int i;
+
+ssize_t byteRecv; // Number of bytes received
+
+ssize_t byteSent; // Number of bytes to be sent
+
+size_t msgLen;
+
+socklen_t serv_size;
+
+char receivedData[MAX_BUF_SIZE]; // Data to be received
+char sendData[MAX_BUF_SIZE]; // Data to be sent
+
+
+
+void *sendThr(void *arg){
+        //now start the cycle for send message
+        for(;;) {
+                printf("Insert message:\n");
+
+                // scanf("%s", sendData);
+                fgets(sendData, MAX_BUF_SIZE, stdin);
+
+                printf("String going to be sent to server: %s", sendData);
+
+                msgLen = countStrLen(sendData);
+                //the action for send message
+                byteSent = sendto(sfd, sendData, msgLen, 0,
+                                  (struct sockaddr *)
+
+                                  &server_addr,
+                                  sizeof(server_addr));
+
+                printf("Bytes sent to server: %zd\n", byteSent);
+        } // end of while return 0;
+        pthread_exit(NULL);
+}
+
+void *recThr(void *arg){
+        //now start the cycle for send message
+        for(;;) {
+                //read relative response from server
+                byteRecv = recvfrom(sfd, receivedData, MAX_BUF_SIZE, 0,
+                                    (struct sockaddr *)&server_addr, &serv_size);
+
+                printf("Received from server: ");
+
+                printData(receivedData, byteRecv);
+                //check if response is equals to goodbye, if this is true close client connection
+                if (strcmp(receivedData, "goodbye") == 0) {
+                        exit(0);
+                }
+                printf("\nInsert message:\n");
+
+        }
+        pthread_exit(NULL);
+}
+
 int main(int argc, char *argv[]) {
+        //check if parameters is what i expective and relative print
+        if (argc == 3) {
+                printf("The IP address supplied is %s\n", argv[1]); // IP
+                SERVER_IP = argv[1];
+                printf("The Port number supplied is %s\n", argv[2]); // PORTA
+                SERVER_PORT = strtol(argv[2], NULL, 10);
+        } else if (argc > 3) {
+                printf("Too many arguments supplied.\n");
+                exit(0);
+        } else {
+                printf("Two argument expected [IP]&[PORT].\n");
+                exit(0);
+        }
 
-  //Variable for connection to server, ip and port
-  int SERVER_PORT;
-  char *SERVER_IP;
+        //open socket
+        sfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-  //Check if parameters is what i expective and relative print
-  if (argc == 3) {
-    printf("The argument supplied is %s\n", argv[1]); // IP
-    SERVER_IP = argv[1];
-    printf("The argument supplied is %s\n", argv[2]); // PORTA
-    SERVER_PORT = strtol(argv[2], NULL, 10);
-  } else if (argc > 3) {
-    printf("Too many arguments supplied.\n");
-    exit(0);
-  } else {
-    printf("Two argument expected.\n");
-    exit(0);
-  }
+        if (sfd < 0) {
+                perror("socket"); // Print error message
+                exit(EXIT_FAILURE);
+        }
 
-  //Now start configuration parameters for client
-  struct sockaddr_in
-      server_addr; // struct containing server address information
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(SERVER_PORT);
+        server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
-  struct sockaddr_in
-      client_addr; // struct containing client address information
+        serv_size = sizeof(server_addr);
 
-  int sfd; // Server socket filed descriptor
+        pthread_t th;
+        int rc, i=1, *intptr;
 
-  int br; // Bind result
+        //Sender
+        intptr=malloc(sizeof(int));
+        if( !intptr ) { printf("malloc failed\n"); exit(1); }
+        *intptr=i;
+        rc=pthread_create( &th,NULL,sendThr,(void*)intptr);
+        i++;
 
-  int i;
+        //Receiver
+        intptr=malloc(sizeof(int));
+        if( !intptr ) { printf("malloc failed\n"); exit(1); }
+        *intptr=i;
+        rc=pthread_create( &th,NULL,recThr,(void*)intptr);
 
-  int stop = 0;
+        pthread_exit(NULL);
 
-  ssize_t byteRecv; // Number of bytes received
-
-  ssize_t byteSent; // Number of bytes to be sent
-
-  size_t msgLen;
-
-  socklen_t serv_size;
-
-  char receivedData[MAX_BUF_SIZE]; // Data to be received
-  char sendData[MAX_BUF_SIZE];     // Data to be sent
-
-  //open socket
-  sfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
-  if (sfd < 0) {
-
-    perror("socket"); // Print error message
-
-    exit(EXIT_FAILURE);
-  }
-
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(SERVER_PORT);
-  server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-
-  serv_size = sizeof(server_addr);
-
-  //Now start the cycle for send message
-  while (!stop) {
-
-    printf("Insert message:\n");
-
-    scanf("%s", sendData);
-
-    printf("String going to be sent to server: %s\n", sendData);
-
-    msgLen = countStrLen(sendData);
-    //the action for send message
-    byteSent = sendto(sfd, sendData, msgLen, 0,
-                      (struct sockaddr *)
-
-                      &server_addr,
-                      sizeof(server_addr));
-
-    printf("Bytes sent to server: %zd\n", byteSent);
-    //read relative response from server
-    byteRecv = recvfrom(sfd, receivedData, MAX_BUF_SIZE, 0,
-                        (struct sockaddr *)&server_addr, &serv_size);
-
-    printf("Received from server: ");
-
-    printData(receivedData, byteRecv);
-    //check if response is euqals to goodbye, if this is true close client connection
-    if (strcmp(receivedData, "goodbye") == 0) {
-      stop = 1;
-    }
-
-  } // End of while return 0;
+        return 0;
 }
--- UDPserver.c
+++ UDPserver.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <pthread.h>
 
 //For inet_toa
 #include <arpa/inet.h>
@@ -14,103 +15,146 @@
 
 #define MAX_BUF_SIZE 1024 // Maximum size of UDP messages
 
+//Configuration variable for server
+int SERVER_PORT;
+char *SERVER_IP;
+
+struct sockaddr_in server_addr; // struct containing server address information
+struct sockaddr_in client_addr; // struct containing client address information
+int sfd; // Server socket filed descriptor
+int br; // Bind result
+int i;
+
+ssize_t byteRecv; // Number of bytes received
+
+ssize_t byteSent; // Number of bytes to be sent
+socklen_t cli_size;
+
+size_t msgLen;
+
+char receivedData[MAX_BUF_SIZE]; // Data to be received
+char sendData[MAX_BUF_SIZE]; // Data to be sent
+
+
+
+void *sendThr(void *arg){
+        //Now start the cycle for send message
+        while (true) {
+                printf("Insert message:\n");
+
+                // scanf("%s", sendData);
+                fgets(sendData, MAX_BUF_SIZE, stdin);
+
+                printf("String going to be sent to client: %s", sendData);
+
+                msgLen = countStrLen(sendData);
+                //the action for send message
+                byteSent = sendto(sfd, sendData, msgLen, 0,
+                                  (struct sockaddr *)
+
+                                  &client_addr,
+                                  sizeof(client_addr));
+                if(byteSent==-1) {
+                        printf("!!!Server can't send message to client if this hasn't sent something before!!!\n");
+                }else{
+                        printf("Bytes sent to client: %zd\n", byteSent);
+                }
+
+        } // End of while return 0;
+        pthread_exit(NULL);
+}
+
+void *recThr(void *arg){
+        //cycle for receive message, in this case we can see the gestion of message is sequencile
+        for (;;) {
+                //read message
+                byteRecv = recvfrom(sfd, receivedData, MAX_BUF_SIZE, 0,
+                                    (struct sockaddr *)&client_addr, &cli_size);
+                //check if message is "normal" or a error
+                if (byteRecv == -1) {
+                        perror("recvfrom");
+                        exit(EXIT_FAILURE);
+                }
+
+                printf("Received data Socket(%s:%d): \n", inet_ntoa(client_addr.sin_addr),
+                       client_addr.sin_port);
+                printData(receivedData, byteRecv);
+                printf("Insert message:\n");
+
+                //compare if data receive is equals to exit,
+                //in that case we response with "goodbye" string
+                //in all case we respond with the thame message of client
+                if (strncmp(receivedData, "exit\n", byteRecv) == 0) {
+
+                        byteSent = sendto(sfd, "goodbye", 7, 0,
+                                          (struct sockaddr *)
+
+                                          &client_addr,
+                                          sizeof(client_addr));
+//                } else {
+//
+//                        printf("Response to be sent back to client: ");
+//                        printData(receivedData, byteRecv);
+//                       byteSent = sendto(sfd, receivedData, byteRecv, 0,
+//                                          (struct sockaddr *)
+//
+//                                          &client_addr,
+//                                          sizeof(client_addr));
+                }
+
+        } // End of for(;;) return 0;
+}
+
 int main(int argc, char *argv[]) {
-  //Configuration variable for server
-  int SERVER_PORT;
-  char *SERVER_IP;
+        //Check if parameters is what i expective and relative print
+        if (argc == 3) {
+                printf("The IP address supplied is %s\n", argv[1]); // IP
+                SERVER_IP = argv[1];
+                printf("The Port number supplied is %s\n", argv[2]); // PORTA
+                SERVER_PORT = strtol(argv[2], NULL, 10);
+        } else if (argc > 3) {
+                printf("Too many arguments supplied.\n");
+                exit(0);
+        } else {
+                printf("Two argument expected [IP]&[PORT].\n");
+                exit(0);
+        }
 
-  //Check if parameters is what i expective and relative print
-  if (argc == 3) {
-    printf("the argoument supplied is IP %s\n", argv[1]); // IP
-    SERVER_IP = argv[1];
-    printf("the argoument supplied is PORT %s\n", argv[2]); // PORT
-    SERVER_PORT = strtol(argv[2], NULL, 10);
-  } else if (argc > 3) {
-    printf("too many arguments \n");
-    exit(0);
-  } else {
-    printf("two arguments expected \n");
-    exit(0);
-  }
+        sfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-  struct sockaddr_in
-      server_addr; // struct containing server address information
+        if (sfd < 0) {
+                perror("socket"); // Print error message
+                exit(EXIT_FAILURE);
+        }
 
-  struct sockaddr_in
-      client_addr; // struct containing client address information
+        //• Initialize server address information
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(SERVER_PORT); // Convert to network byte order
+        server_addr.sin_addr.s_addr = INADDR_ANY; // Bind to any address
 
-  int sfd; // Server socket filed descriptor
+        br = bind(sfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        if (br < 0) {
+                perror("bind"); // Print error message
+                exit(EXIT_FAILURE);
+        }
+        cli_size = sizeof(client_addr);
 
-  int br; // Bind result
+        pthread_t th;
+        int rc, i=1, *intptr;
 
-  int i;
+        //Sender
+        intptr=malloc(sizeof(int));
+        if( !intptr ) { printf("malloc failed\n"); exit(1); }
+        *intptr=i;
+        rc=pthread_create( &th,NULL,sendThr,(void*)intptr);
+        i++;
 
-  ssize_t byteRecv; // Number of bytes received
+        //Receiver
+        intptr=malloc(sizeof(int));
+        if( !intptr ) { printf("malloc failed\n"); exit(1); }
+        *intptr=i;
+        rc=pthread_create( &th,NULL,recThr,(void*)intptr);
 
-  ssize_t byteSent; // Number of bytes to be sent
-  socklen_t cli_size;
-
-  char receivedData[MAX_BUF_SIZE]; // Data to be received
-  char sendData[MAX_BUF_SIZE];     // Data to be sent
-
-  sfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
-  if (sfd < 0) {
-
-    perror("socket"); // Print error message
-
-    exit(EXIT_FAILURE);
-  }
-
-  //• Initialize server address information
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(SERVER_PORT); // Convert to network byte order
-  server_addr.sin_addr.s_addr = INADDR_ANY; // Bind to any address
-
-  br = bind(sfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  if (br < 0) {
-
-    perror("bind"); // Print error message
-
-    exit(EXIT_FAILURE);
-  }
-  cli_size = sizeof(client_addr);
-
-  //cycle for receive message, in this case we can see the gestion of message is sequencile
-  for (;;) {
-    //read message
-    byteRecv = recvfrom(sfd, receivedData, MAX_BUF_SIZE, 0,
-                        (struct sockaddr *)&client_addr, &cli_size);
-    //check if message is "normal" or a error
-    if (byteRecv == -1) {
-      perror("recvfrom");
-      exit(EXIT_FAILURE);
-    }
-
-    printf("Received data Socket(%s:%d): \n", inet_ntoa(client_addr.sin_addr),
-               client_addr.sin_port);
-    printData(receivedData, byteRecv);
-
-    //compare if data receive is equals to exit,
-    //in that case we response with "goodbye" string
-    //in all case we respond with the thame message of client
-    if (strncmp(receivedData, "exit", byteRecv) == 0) {
-
-      byteSent = sendto(sfd, "goodbye", 7, 0,
-                        (struct sockaddr *)
-
-                        &client_addr,
-                        sizeof(client_addr));
-    } else {
-
-      printf("Response to be sent back to client: ");
-      printData(receivedData, byteRecv);
-      byteSent = sendto(sfd, receivedData, byteRecv, 0,
-                        (struct sockaddr *)
-
-                        &client_addr,
-                        sizeof(client_addr));
-    }
-
-  } // End of for(;;) return 0;
+        pthread_exit(NULL);
+        return 0;
 }
